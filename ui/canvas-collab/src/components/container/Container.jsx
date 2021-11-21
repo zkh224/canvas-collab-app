57cb243697c763649e1dd7184a1dd54965105660
zkh224 <khanzahra06@hotmail.com>
2021-11-21 03:21:54 -0500

    code init

---
+++ ui/canvas-collab/src/components/container/Container.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Canvas from '../canvas/Canvas'
+import './style.css';
+
+class Container extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div className="container">
+                <div className="color-picker">
+                    <input type="color" />
+                </div>
+
+                <div class="canvas-container">
+                    <Canvas></Canvas>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default Container
