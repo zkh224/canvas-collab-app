012d837d48a0b099fccd452069d7129a1a23f594
zkh224 <khanzahra06@hotmail.com>
2021-11-23 18:32:41 -0500

    Added touch functionality

--- ui/canvas-collab/src/components/canvas/Canvas.jsx
+++ ui/canvas-collab/src/components/canvas/Canvas.jsx
@@ -85,6 +85,57 @@ class Board extends React.Component {
       false
     );
 
+
+    // Set up touch events for mobile, etc
+      canvas.addEventListener("touchstart", function (e) {
+        mouse = getTouchPos(canvas, e);
+      var touch = e.touches[0];
+      var mouseEvent = new MouseEvent("mousedown", {
+      clientX: touch.clientX,
+      clientY: touch.clientY
+      });
+      canvas.dispatchEvent(mouseEvent);
+      }, false);
+      canvas.addEventListener("touchend", function (e) {
+      var mouseEvent = new MouseEvent("mouseup", {});
+      canvas.dispatchEvent(mouseEvent);
+      }, false);
+      canvas.addEventListener("touchmove", function (e) {
+      var touch = e.touches[0];
+      var mouseEvent = new MouseEvent("mousemove", {
+      clientX: touch.clientX,
+      clientY: touch.clientY
+      });
+      canvas.dispatchEvent(mouseEvent);
+      }, false);
+
+      // Get the position of a touch relative to the canvas
+      function getTouchPos(canvasDom, touchEvent) {
+      var rect = canvasDom.getBoundingClientRect();
+      return {
+      x: touchEvent.touches[0].clientX - rect.left,
+      y: touchEvent.touches[0].clientY - rect.top
+      };
+      }
+
+// Prevent scrolling when touching the canvas
+    document.body.addEventListener("touchstart", function (e) {
+      if (e.target === canvas) {
+        e.preventDefault();
+      }
+    }, false);
+    document.body.addEventListener("touchend", function (e) {
+      if (e.target === canvas) {
+        e.preventDefault();
+      }
+    }, false);
+    document.body.addEventListener("touchmove", function (e) {
+      if (e.target === canvas) {
+        e.preventDefault();
+      }
+    }, false);
+
+
     var root = this;
     var onPaint = function () {
       ctx.beginPath();
