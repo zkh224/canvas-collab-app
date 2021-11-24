fc5ad315da25c6325e193c7ad0779fb5d78144d1
dretoba91e <64743870+dretoba91@users.noreply.github.com>
2021-11-24 10:01:18 +0100

    fixed the multi-user collaboration

--- ui/canvas-collab/src/components/canvas/Canvas.jsx
+++ ui/canvas-collab/src/components/canvas/Canvas.jsx
@@ -5,7 +5,7 @@ import "./style.css";
 
 class Canvas extends React.Component {
   timeout;
-  socket = io.connect("http://localhost:3000");
+  socket = io.connect("http://localhost:4000");
 
   ctx;
   isDrawing = false;
@@ -89,56 +89,78 @@ class Canvas extends React.Component {
       false
     );
 
-
     // Set up touch events for mobile, etc
-      canvas.addEventListener("touchstart", function (e) {
+    canvas.addEventListener(
+      "touchstart",
+      function (e) {
         mouse = getTouchPos(canvas, e);
-      var touch = e.touches[0];
-      var mouseEvent = new MouseEvent("mousedown", {
-      clientX: touch.clientX,
-      clientY: touch.clientY
-      });
-      canvas.dispatchEvent(mouseEvent);
-      }, false);
-      canvas.addEventListener("touchend", function (e) {
-      var mouseEvent = new MouseEvent("mouseup", {});
-      canvas.dispatchEvent(mouseEvent);
-      }, false);
-      canvas.addEventListener("touchmove", function (e) {
-      var touch = e.touches[0];
-      var mouseEvent = new MouseEvent("mousemove", {
-      clientX: touch.clientX,
-      clientY: touch.clientY
-      });
-      canvas.dispatchEvent(mouseEvent);
-      }, false);
+        var touch = e.touches[0];
+        var mouseEvent = new MouseEvent("mousedown", {
+          clientX: touch.clientX,
+          clientY: touch.clientY,
+        });
+        canvas.dispatchEvent(mouseEvent);
+      },
+      false
+    );
+    canvas.addEventListener(
+      "touchend",
+      function (e) {
+        var mouseEvent = new MouseEvent("mouseup", {});
+        canvas.dispatchEvent(mouseEvent);
+      },
+      false
+    );
+    canvas.addEventListener(
+      "touchmove",
+      function (e) {
+        var touch = e.touches[0];
+        var mouseEvent = new MouseEvent("mousemove", {
+          clientX: touch.clientX,
+          clientY: touch.clientY,
+        });
+        canvas.dispatchEvent(mouseEvent);
+      },
+      false
+    );
 
-      // Get the position of a touch relative to the canvas
-      function getTouchPos(canvasDom, touchEvent) {
+    // Get the position of a touch relative to the canvas
+    function getTouchPos(canvasDom, touchEvent) {
       var rect = canvasDom.getBoundingClientRect();
       return {
-      x: touchEvent.touches[0].clientX - rect.left,
-      y: touchEvent.touches[0].clientY - rect.top
+        x: touchEvent.touches[0].clientX - rect.left,
+        y: touchEvent.touches[0].clientY - rect.top,
       };
-      }
+    }
 
-// Prevent scrolling when touching the canvas
-    document.body.addEventListener("touchstart", function (e) {
-      if (e.target === canvas) {
-        e.preventDefault();
-      }
-    }, false);
-    document.body.addEventListener("touchend", function (e) {
-      if (e.target === canvas) {
-        e.preventDefault();
-      }
-    }, false);
-    document.body.addEventListener("touchmove", function (e) {
-      if (e.target === canvas) {
-        e.preventDefault();
-      }
-    }, false);
-
+    // Prevent scrolling when touching the canvas
+    document.body.addEventListener(
+      "touchstart",
+      function (e) {
+        if (e.target === canvas) {
+          e.preventDefault();
+        }
+      },
+      false
+    );
+    document.body.addEventListener(
+      "touchend",
+      function (e) {
+        if (e.target === canvas) {
+          e.preventDefault();
+        }
+      },
+      false
+    );
+    document.body.addEventListener(
+      "touchmove",
+      function (e) {
+        if (e.target === canvas) {
+          e.preventDefault();
+        }
+      },
+      false
+    );
 
     var root = this;
     var onPaint = function () {
@@ -159,8 +181,7 @@ class Canvas extends React.Component {
   render() {
     return (
       <div className="draw" id="draw">
-        <canvas className="canvas" id="canvas">
-        </canvas>
+        <canvas className="canvas" id="canvas"></canvas>
       </div>
     );
   }
